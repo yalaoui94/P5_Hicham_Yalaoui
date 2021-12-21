351e7774760ca279fa957979f8d7ab0df74dd15b
yalaouihicham <yalaouihicham13@gmail.com>
2021-12-21 12:46:10 +0100

    js init

---
+++ js/index.js
@@ -0,0 +1,36 @@
+//initialisation des variables 
+let product = document.getElementById('items')
+
+
+//appel de l'API
+fetch ("http://localhost:3000/api/products")
+.then(function (res) {
+    return res.json()
+})
+.then(function (articles) {
+    articles.forEach( (kanap) => itemDisplay(kanap))
+})
+.catch(function (error){
+    alert( "Une erreur est survenue:"+error)
+})
+
+
+
+//appel lien URL et intégration dans le HTML 
+function itemDisplay (kanap){ 
+    console.log("ok");
+    let newA =document.createElement("a");
+    newA.setAttribute("href",'./product.html?id='+kanap.id);
+
+    newA.innerHTML=
+    `
+    <article>
+    <img src="${kanap.imageUrl}"alt="${kanap.altTxt}">
+    <h3 class="productName">"${kanap.name}"</h3>
+    <p class="productDescription">"${kanap.description}"</p>
+    </article>
+    
+    `
+    product.appendChild(newA)
+    console.log(newA)
+}
